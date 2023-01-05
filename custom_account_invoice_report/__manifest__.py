42259593d8425d14687d5a571abfa745ac3bf881
Tech Consulting <94559660+M22TechConsulting@users.noreply.github.com>
2023-01-05 16:45:45 -0600

    Add files via upload

---
+++ custom_account_invoice_report/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Reporte de facturas personalizado",
+    'summary': """
+        Personalización de reporte de facturas.
+    """,
+    'description': """
+        Personalización de reporte de facturas con un formato diferente al usado por odoo.
+    """,
+    'author': "M22",
+    'website': "https://m22.mx",
+    'category': 'Contabilidad',
+    'version': '16.0.1',
+    'depends': ['base','stock_account','account','l10n_mx_edi','l10n_mx_edi_40'],
+    'data': [
+        'reports/report_invoice_document.xml'
+    ],
+    'license': 'AGPL-3'
+}
