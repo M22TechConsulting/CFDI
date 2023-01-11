e4ed5b3538737d2c6c38a976fb100e1ad0d32c3a
Tech Consulting <94559660+M22TechConsulting@users.noreply.github.com>
2023-01-11 12:20:37 -0600

    Add files via upload

--- custom_account_invoice_report/__manifest__.py
+++ custom_account_invoice_report/__manifest__.py
@@ -11,11 +11,16 @@
     'website': "https://m22.mx",
     'category': 'Contabilidad',
     'version': '16.0.1',
-    'depends': ['base','sale','stock_account','l10n_mx_edi_40'],
+    'depends': ['base', 'sale', 'stock_account', 'l10n_mx_edi_40'],
     'data': [
         'reports/report_header_document.xml',
         'reports/report_invoice_document.xml',
         'reports/report_saleorder_pro_forma.xml'
     ],
+    'assets': {
+        'web.report_assets_common': [
+            'custom_account_invoice_report/static/src/scss/style.scss',
+        ],
+    },
     'license': 'AGPL-3'
 }
