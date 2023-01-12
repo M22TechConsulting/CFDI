65c1712dd4f7580d14e2701534b1601ddafbad3a
Tech Consulting <94559660+M22TechConsulting@users.noreply.github.com>
2023-01-11 22:02:46 -0700

    Add files via upload

--- custom_account_invoice_report/models/sale_order.py
+++ custom_account_invoice_report/models/sale_order.py
@@ -11,5 +11,24 @@ class SaleOrder(models.Model):
                 lot_names += lot_ids
         return lot_names
 
+    @api.model
+    def _amount_to_text_proforma(self):
+        self.ensure_one()
+
+        currency_name = self.currency_id.name.upper()
+
+        currency_type = 'M.N' if currency_name == 'MXN' else 'M.E.'
+
+        amount_i, amount_d = divmod(self.amount_total, 1)
+        amount_d = round(amount_d, 2)
+        amount_d = int(round(amount_d * 100, 2))
+
+        words = self.currency_id.with_context(lang=self.partner_id.lang or 'es_ES').amount_to_text(amount_i).upper()
+        return '%(words)s %(amount_d)02d/100 %(currency_type)s' % {
+            'words': words,
+            'amount_d': amount_d,
+            'currency_type': currency_type,
+        }
+
 
 
